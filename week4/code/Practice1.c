a0e4bd73217bf4e26002b7eff33fa29994a57d82
Donghee Kang <65123546+dhk010111@users.noreply.github.com>
2023-04-10 18:01:37 +0900

    week4 upload

---
+++ week4/code/Practice1.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int global = 10;
+int main(){
+	static int i = 100;
+	return 0;
+}
---
+++ week4/code/smile.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	int a = 85;
+	int b = 15;
+	int sum = a + b;
+
+	if (sum != (a+b)){
+		printf("a+b와 sum값이 같습니다:)");
+	}
+	else {
+		printf("a+b와 sum값이 다릅니다!!");
+	}
+	return 0;
+}
