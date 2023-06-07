03b90d86c1219b10bd8171e95330dd5c0b11b03e
Donghee Kang <65123546+dhk010111@users.noreply.github.com>
2023-06-07 10:18:14 +0900

    Delete week11/컴퓨터보안 실습 11주차(추가자료) directory

--- week11/컴퓨터보안 실습 11주차(추가자료)/프로젝트3_실습코드/8-6 fuzzingBlazeDVD.py
+++
@@ -1,7 +0,0 @@
-# 'junk' 변수에 500바이트 크기의 문자열 "\x41" (16진수로 41은 'A'에 해당)을 반복하여 저장한다.
-junk ="\x41"*500 
-
-# 'blazeExpl.plf' 파일을 쓰기 모드로 열고, 'junk' 문자열을 파일에 기록한다.
-x=open('blazeExpl.plf', 'w')
-x.write(junk)
-x.close()
